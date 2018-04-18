bc7f094ecdc16f0b3719c6a2b4169376ee345148
yasminhillis <jasminehillis94@gmail.com>
2018-04-18 16:19:12 +0300

    add fileStructure

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    
+};
